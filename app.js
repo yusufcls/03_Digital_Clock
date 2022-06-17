12d9b7ede68112eeb0d97208b19c6ff6962e5f71
Yusuf C <yusufcfly@gmail.com>
2022-06-17 20:49:18 +0200

    AM_PM

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const time = document.querySelector(".time");
 
 function digitalClock () {
     let dateTime = new Date();
-    let clock = dateTime.toLocaleTimeString();
+    let clock = dateTime.toLocaleTimeString('en-US');
     time.innerText = clock;
+    console.log(clock);
 }
 
 setInterval(digitalClock, 1000);
